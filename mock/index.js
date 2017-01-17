69202b8ef538821c7aa912e9bc9abc9185565885
hzyubaoquan <hzyubaoquan@corp.netease.com>
2017-01-17 15:27:44 +0800

    Replace EventEmitter with Promise.all
    bugfix: when EventEmitter emit the msg so early that onMockFinish is not
    invoked yet, the app will block.

--- mock/index.js
+++ mock/index.js
@@ -7,11 +7,8 @@ const logUtil = require('../util/logUtil');
 const events = require('events');
 const extend = require('node.extend');
 
-var initFinished = 0;
-var eventEmitter = new events.EventEmitter();
-
-arr.forEach((page) => {
-    mock(page);
+let tasks = arr.map((page) => {
+    return mock(page);
 });
 
 function isFrontPage(url) {
@@ -19,52 +16,46 @@ function isFrontPage(url) {
 }
 
 function mock(page) {
-    page.ftlData = {};
+    return new Promise((resolve, reject) => {
+        page.ftlData = {};
 
-    //初始化公用ftl数据
-    extend(page.ftlData, commonData);
+        //初始化公用ftl数据
+        extend(page.ftlData, commonData);
 
-    if (isFrontPage(page.entry)) {
-        countInitedNum();
-        return;
-    }
+        if (isFrontPage(page.entry)) {
+            resolve();
+            return;
+        }
 
-    //初始化同步接口数据
-    let ftlMockFilePath = __dirname + '/ftlMockData/' + page.entry.slice(1).replace(/\//g, '.') + '.json';
-    try {
-        extend(page.ftlData, require(ftlMockFilePath));
-    } catch(e) {
-        //console.info('no ftl data, pass');
-    }
+        //初始化同步接口数据
+        let ftlMockFilePath = __dirname + '/ftlMockData/' + page.entry.slice(1).replace(/\//g, '.') + '.json';
+        try {
+            extend(page.ftlData, require(ftlMockFilePath));
+        } catch(e) {
+            //console.info('no ftl data, pass');
+        }
 
-    //初始化异步接口数据
-    page.ajax = [];
-    let relativePath = page.entry.slice(1).replace(/\//g, '.');
-    let ajaxFolderPath = __dirname + '/ajax/' + relativePath;
+        //初始化异步接口数据
+        page.ajax = [];
+        let relativePath = page.entry.slice(1).replace(/\//g, '.');
+        let ajaxFolderPath = __dirname + '/ajax/' + relativePath;
 
-    fileUtil.listFiles(ajaxFolderPath)
-    .then((fileNames) => {
-        fileNames.forEach((fileName) => {
-            let modulePath = `${ajaxFolderPath}/${fileName}`;
-            let json = require(modulePath);
-            page.ajax.push(json);
+        // 限制文件返回格式为json; 可以过滤mac中的隐藏文件, 如.DSstore, 防止读取ajax配置失败
+        fileUtil.listFiles(ajaxFolderPath, item => /\.json/.test(item))
+        .then((fileNames) => {
+            fileNames.forEach((fileName) => {
+                let json = require(`${ajaxFolderPath}/${fileName}`);
+                page.ajax.push(json);
+            });
+            resolve();
+        }).catch(() => {
+            //console.info(`读取ajax配置失败, 跳过${relativePath}`);
+            resolve();
         });
-        countInitedNum();
-    }).catch(() => {
-        console.info(`读取ajax配置失败, 跳过${relativePath}`);
-        countInitedNum();
     });
-}
-
-/**
- * 记录初始化完成的页面数
- */
-function countInitedNum() {
-    initFinished ++;
-    (initFinished == arr.length) && eventEmitter.emit('initFinished');
 }
 
 module.exports = arr;
 module.exports.onMockFinish = function(cb) {
-    eventEmitter.on('initFinished', cb);
+    Promise.all(tasks).then(cb);
 };
