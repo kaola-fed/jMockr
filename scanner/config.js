d026c8839942da2b3c38005725d016b36648cdb5
hzyubaoquan <hzyubaoquan@corp.netease.com>
2017-03-01 22:17:20 +0800

    wtf

--- scanner/config.js
+++ scanner/config.js
@@ -1,4 +1,5 @@
 const path = require('path');
 const fileUtil = require('../util/fileUtil');
+const config = fileUtil.json5Require(path.resolve(__dirname, '../jmockr.config.json'));
 
 module.exports = config;
