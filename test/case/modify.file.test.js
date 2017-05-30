551d669f2e2d51e0633a7cc9e2a7825dbe6b79e6
yubaoquan <hzyubaoquan@corp.netease.com>
2017-05-30 18:33:03 +0800

    Command failed: npm run tests.Not resolved.

--- test/case/modify.file.test.js
+++ test/case/modify.file.test.js
@@ -104,8 +104,34 @@ async function timeout(ms) {
 //     await timeout(1000);
 //     expect(1).toBeTruthy();
 // });
+var psTree = require('ps-tree');
+
+var kill = function (pid, signal, callback) {
+    signal   = signal || 'SIGKILL';
+    callback = callback || function () {};
+    var killTree = true;
+    if(killTree) {
+        psTree(pid, function (err, children) {
+            [pid].concat(
+                children.map(function (p) {
+                    return p.PID;
+                })
+            ).forEach(function (tpid) {
+                try { process.kill(tpid, signal) }
+                catch (ex) { }
+            });
+            callback();
+        });
+    } else {
+        try { process.kill(pid, signal) }
+        catch (ex) { }
+        callback();
+    }
+};
+
 afterAll(() => {
-    childProcess.kill('SIGTERM');
+    // childProcess.kill('SIGTERM');
+    kill(childProcess.pid);
     // process.kill(childProcess.pid, 'SIGTERM');
     fs.writeFile(filePath, JSON.stringify(originURLs).replace(/^"|"$/g, ''));
 });
